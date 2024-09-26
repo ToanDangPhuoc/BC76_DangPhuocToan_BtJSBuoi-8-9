e8b4cc496345b8eb25da517f8bc65cd516fe77ad
toandang <toandangphuoc9991@gmail.com>
2024-09-26 23:42:19 +0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,118 @@
+let arrNhanVien = [];
+
+// thêm nhân viên
+document.getElementById("form-content").onsubmit = function (event) {
+  event.preventDefault();
+  console.log("hello");
+  let arrField = document.querySelectorAll(
+    "#form-content input, #form-content select"
+  );
+  console.log(arrField);
+  let nhanVien = new NhanVien();
+  for (let field of arrField) {
+    let { value, id } = field;
+    nhanVien[id] = value;
+  }
+  console.log(nhanVien);
+  arrNhanVien.push(nhanVien);
+  setDataLocal("arrNhanVien", arrNhanVien);
+  renderData();
+};
+// Hiển thị lên giao diện
+
+function renderData(arr = arrNhanVien) {
+  let content = "";
+  for (let nhanVien of arr) {
+    let newNhanVien = new NhanVien();
+    Object.assign(newNhanVien, nhanVien);
+    let { tknv, name, email, datepicker, chucvu, tinhTongLuong, loaiNhanVien } =
+      newNhanVien;
+    content += `
+    <tr> 
+    <td>${tknv}</td>
+    <td>${name}</td>
+    <td>${email}</td>
+    <td>${datepicker}</td>
+    <td>${chucvu}</td>
+    <td>$${newNhanVien.tinhTongLuong()}</td>
+    <td>${newNhanVien.loaiNhanVien()}</td>
+    <td>
+    <button onclick="xoaNhanVien('${tknv}')" class="btn btn-danger">Xóa</button>
+   <button id="openModalBtn" class="btn btn-warning" onclick="openModal('${tknv}')">Sửa</button>
+    </td>
+    </tr>`;
+  }
+  document.getElementById("tableDanhSach").innerHTML = content;
+}
+
+window.onload = function () {
+  let dataLocal = getDataLocal("arrNhanVien");
+  if (dataLocal) {
+    arrNhanVien = dataLocal;
+    renderData();
+  }
+};
+// local storage'
+
+//---function setdatalocal---
+function setDataLocal(key, value) {
+  let dataString = JSON.stringify(value);
+  localStorage.setItem(key, dataString);
+}
+//---function getdatalocal---
+function getDataLocal(key) {
+  let dataLocal = localStorage.getItem(key);
+  return dataLocal ? JSON.parse(dataLocal) : null;
+}
+
+//---function xóa ----
+function xoaNhanVien(tknv) {
+  let index = arrNhanVien.findIndex((item, i) => item.tknv == tknv); // -1
+  if (index != -1) {
+    arrNhanVien.splice(index, 1);
+    renderData();
+    setDataLocal("arrNhanVien", arrNhanVien);
+  }
+}
+
+// --- function sửa---
+function openModal(tknv) {
+  document.getElementById("myModal").style.display = "block"; // Hiển thị modal
+  getInfoNhanVien(tknv); // Gọi hàm lấy thông tin nhân viên khi mở modal
+}
+
+function getInfoNhanVien(tknv) {
+  document.getElementById("myModal").style.display = "block";
+  console.log(tknv);
+
+  let nhanVien = arrNhanVien.find((item, index) => item.tknv == tknv); // sinhVien || undifined
+  if (nhanVien) {
+    let arrField = document.querySelectorAll(
+      "#form-content  input, #form-content select"
+    );
+    for (let field of arrField) {
+      // field đại diện cho các input và select tìm kiếm được trong form
+      field.value = nhanVien[field.id];
+      if (field.id == "tknv") {
+        field.readOnly = true;
+      }
+    }
+  }
+}
+
+// tìm kiếm nhân viên
+document.getElementById("btnTimNV").oninput = function (event) {
+  let keyword = event.target.value.trim().toLowerCase();
+  console.log(keyword);
+  let newKeyword = removeVietnameseTones(keyword);
+
+  let arrSearch = arrNhanVien.filter((item, index) => {
+    // item.txtTenSv =" Quang Khai" newKeyword="Quang"
+
+    let loaiNhanVien = removeVietnameseTones(
+      item.txtTenSV.trim().toLowerCase()
+    );
+    return newTenSinhVien.includes(newKeyword);
+  });
+  renderDataSinhVien(arrSearch);
+};
---
+++ model/NhanVien.js
@@ -0,0 +1,42 @@
+class NhanVien {
+  tknv = "";
+  name = "";
+  email = "";
+  password = "";
+  datepicker = "";
+  luongCB = "";
+  chucvu = "";
+  gioLam = "";
+  tinhTongLuong = function () {
+    let tongLuong = 0;
+    switch (this.chucvu) {
+      case "Giám đốc":
+        tongLuong = this.luongCB * 1 * 3;
+        break;
+      case "Trưởng phòng":
+        tongLuong = this.luongCB * 1 * 2;
+        break;
+      case "Nhân viên":
+        tongLuong = this.luongCB * 1;
+        break;
+    }
+    return tongLuong;
+  };
+  loaiNhanVien = function () {
+    let xepLoai = "";
+    switch (true) {
+      case this.gioLam >= 192:
+        xepLoai = "Xuất sắc";
+        break;
+      case this.gioLam >= 176:
+        xepLoai = "Giỏi";
+        break;
+      case this.gioLam >= 160:
+        xepLoai = "Khá";
+        break;
+      case this.gioLam < 160:
+        xepLoai = "Trung bình";
+    }
+    return xepLoai;
+  };
+}
